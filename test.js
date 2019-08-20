86e9f8ed02b8b9be1fdf7892f931819009d7ce28
Johny Kim <johnyworld@naver.com>
2019-08-20 12:37:22 +0900

    Lecture Done

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth()+1);
+console.log(new Date().getDate());
+console.log(new Date().getDay());
+console.log(new Date().getHours());
+console.log(new Date().getMinutes());
+console.log(new Date().getSeconds());
+console.log(new Date().getTime());
+
